37cecbfeabb41aaaae8282d524a27a63549b8d3e
David Bakare <bakaredavid007@gmail.com>
2023-11-01 02:41:13 +0100

    Even more comments

--- app/components/MenuBar.tsx
+++ app/components/MenuBar.tsx
@@ -1,53 +1,53 @@
-import menu from "../../public/menu.svg";
-import { Sheet, SheetContent, SheetTrigger } from "@/app/components/ui/sheet";
+// import menu from "../../public/menu.svg";
+// import { Sheet, SheetContent, SheetTrigger } from "@/app/components/ui/sheet";
 
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
-import { Twitter } from "@/app/components/logos/Twitter";
-import { LinkedIn } from "@/app/components/logos/LinkedIn";
+// import { cn } from "@/lib/utils";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { Twitter } from "@/app/components/logos/Twitter";
+// import { LinkedIn } from "@/app/components/logos/LinkedIn";
 
-function MenuBar() {
-  return (
-    <Sheet>
-      <SheetTrigger className={cn("sm:hidden")}>
-        <Image
-          src={menu}
-          alt="menu"
-          className="sm:hidden dark:filter dark:invert"
-        />
-      </SheetTrigger>
-      <SheetContent
-        side={"left"}
-        className={cn("flex items-center justify-center sm:hidden")}
-      >
-        <aside className="space-y-20">
-          <ul className="space-y-20 text-xl text-foreground flex flex-col items-center">
-            <Link href="#features" className="">
-              Features
-            </Link>
-            {/* <Link href="/" className="" aria-disabled>
-              Pricing
-            </Link> */}
-            <Link href="#about" className="">
-              About
-            </Link>
-            <Link href="/" className="">
-              Terms & Conditions
-            </Link>
-          </ul>
-          <div className="gap-8 items-center justify-center flex">
-            <Link href="">
-              <Twitter className="w-6 h-6" />
-            </Link>
-            <Link href="">
-              <LinkedIn className="w-6 h-6" />
-            </Link>
-          </div>
-        </aside>
-      </SheetContent>
-    </Sheet>
-  );
-}
+// function MenuBar() {
+//   return (
+//     <Sheet>
+//       <SheetTrigger className={cn("sm:hidden")}>
+//         <Image
+//           src={menu}
+//           alt="menu"
+//           className="sm:hidden dark:filter dark:invert"
+//         />
+//       </SheetTrigger>
+//       <SheetContent
+//         side={"left"}
+//         className={cn("flex items-center justify-center sm:hidden")}
+//       >
+//         <aside className="space-y-20">
+//           <ul className="space-y-20 text-xl text-foreground flex flex-col items-center">
+//             <Link href="#features" className="">
+//               Features
+//             </Link>
+//             {/* <Link href="/" className="" aria-disabled>
+//               Pricing
+//             </Link> */}
+//             <Link href="#about" className="">
+//               About
+//             </Link>
+//             <Link href="/" className="">
+//               Terms & Conditions
+//             </Link>
+//           </ul>
+//           <div className="gap-8 items-center justify-center flex">
+//             <Link href="">
+//               <Twitter className="w-6 h-6" />
+//             </Link>
+//             <Link href="">
+//               <LinkedIn className="w-6 h-6" />
+//             </Link>
+//           </div>
+//         </aside>
+//       </SheetContent>
+//     </Sheet>
+//   );
+// }
 
-export default MenuBar;
+// export default MenuBar;
