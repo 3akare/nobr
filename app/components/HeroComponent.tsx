5cf6692b52d037eb3a3bfe65dc6fdb94b40cb1e1
David Bakare <bakaredavid007@gmail.com>
2023-11-02 23:21:17 +0100

    Color contrast fix

--- app/components/HeroComponent.tsx
+++ app/components/HeroComponent.tsx
@@ -7,7 +7,7 @@ const HeroComponent = () => {
       <div className="container mx-auto p-4 w-full flex flex-col justify-center items-center space-y-10 text-center">
         <Link
           href="/"
-          className="px-4 py-2 rounded-3xl font-medium bg-pOrange w-fit"
+          className="px-4 py-2 rounded-3xl font-medium bg-pOrange w-fit shadow-md dark:text-background"
         >
           Follow along on Twitter
         </Link>
@@ -19,11 +19,14 @@ const HeroComponent = () => {
           aspernatur quam soluta.
         </h2>
         <div className="flex gap-4">
-          <Button className="w-36 sm:w-48 h-14 rounded-xl text-lg hover:text-pOrange hover:bg-secondary">
+          <Button
+            className="w-36 sm:w-48 h-14 rounded-xl text-lg ring-2 ring-primary hover:ring-primary/90 shadow-md
+          "
+          >
             Chat
           </Button>
-          <Button className="w-36 sm:w-48 h-14 rounded-xl text-lg bg-secondary text-pOrange hover:bg-foreground hover:text-background">
-            Chat
+          <Button className="w-36 sm:w-48 h-14 rounded-xl text-lg ring-2 ring-pOrange bg-transparent text-pOrange hover:bg-pOrange hover:text-white shadow-md">
+            Video
           </Button>
         </div>
       </div>
