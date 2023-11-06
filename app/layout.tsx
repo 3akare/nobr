168f4eccca0d9523d3e9d6f99b94b556c833d251
David Bakare <bakaredavid007@gmail.com>
2023-11-06 15:42:06 +0100

    Seo Metadata test

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,62 @@ import { Footer, NavigationBar } from "./components";
 import { Toaster } from "./components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: "nobr | Home",
-  description: "Talk to Strangers",
+  title: "nobr",
+  appleWebApp: { capable: true, statusBarStyle: "default", title: "nobr" },
+  icons: "./public/logos/light-nobr-logo.svg",
+  description:
+    "Discover a safe and fun way to connect with people from around the world",
+  openGraph: {
+    siteName: "nobr",
+    countryName: "Nigeria",
+    description:
+      "Discover a safe and fun way to connect with people from around the world",
+    title: "nobr",
+    url: "https://nobr-delta.vercel.app",
+    type: "website",
+    images: {
+      url: "./public/logos/light-nobr-logo.sv",
+      height: 800,
+      width: 1260,
+    },
+  },
+  twitter: {
+    title: "nobr",
+    creator: "David Bakare",
+    description:
+      "Discover a safe and fun way to connect with people from around the world",
+    images: {
+      url: "./public/logos/light-nobr-logo.sv",
+      height: 800,
+      width: 1260,
+    },
+    card: "summary",
+  },
+  applicationName: "nobr",
+  keywords: [
+    "nobr",
+    "chat",
+    "private",
+    "secure",
+    "secret",
+    "message",
+    "messaging",
+    "hide",
+    "hidden",
+    "confidential",
+    "discreet",
+    "incognito",
+    "online",
+    "untraceable",
+    "strangers",
+    "someone",
+    "friend",
+    "encrypted",
+    "romance",
+    "date",
+    "where",
+    "place",
+  ],
 };
 
 export default function RootLayout({
