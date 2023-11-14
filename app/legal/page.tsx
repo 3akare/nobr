87793907f9986c3967ebbf0264a5a805264ceb75
David Bakare <bakaredavid007@gmail.com>
2023-11-14 16:11:29 +0100

    Fixed Production build error

--- app/legal/page.tsx
+++ app/legal/page.tsx
@@ -6,92 +6,94 @@ const page = () => {
         <section>
           <h1 className="text-2xl font-medium">Terms of service</h1>
           <p>
-            Terms of service Terms of Service for "nobr" 1. Introduction nobr is
-            a web-based platform that allows individuals of all ages, races,
-            gender, and ethnicities to communicate anonymously through text and
-            video chatting. Our platform is designed to provide a safe and
-            secure space for people to connect and share their thoughts and
-            ideas without fear of judgement or reprisal. These terms of service
-            outline the rules and expectations for users of our platform.
-            2.Eligibility Users are required to meet the age requirements set by
-            their local jurisdiction to use "nobr." Minors should seek parental
-            or guardian consent. 3. User Conduct a. Respect and Civility: We
-            expect all users to engage in a respectful and civil manner,
-            refraining from abusive behavior. b. Prohibited Activities:
-            Harassment, cyberbullying, and any form of illegal activities are
-            strictly prohibited. 4. Anonymity While "nobr" offers anonymous
-            chatting, the choice to disclose your identity is at your own risk.
-            "nobr" will not be held responsible for any consequences. 5. Content
-            Guidelines a. Inappropriate Content: Users are prohibited from
-            sharing explicit, offensive, or illegal content. b. Copyright
-            Compliance: Respect copyrights and permissions when sharing content.
-            6. Privacy and Data Usage a. Privacy Commitment: "nobr" values user
+            Terms of service Terms of Service for &quot;nobr&quot; 1.
+            Introduction nobr is a web-based platform that allows individuals of
+            all ages, races, gender, and ethnicities to communicate anonymously
+            through text and video chatting. Our platform is designed to provide
+            a safe and secure space for people to connect and share their
+            thoughts and ideas without fear of judgement or reprisal. These
+            terms of service outline the rules and expectations for users of our
+            platform. 2.Eligibility Users are required to meet the age
+            requirements set by their local jurisdiction to use
+            &quot;nobr.&quot; Minors should seek parental or guardian consent.
+            3. User Conduct a. Respect and Civility: We expect all users to
+            engage in a respectful and civil manner, refraining from abusive
+            behavior. b. Prohibited Activities: Harassment, cyberbullying, and
+            any form of illegal activities are strictly prohibited. 4. Anonymity
+            While &quot;nobr&quot; offers anonymous chatting, the choice to
+            disclose your identity is at your own risk. &quot;nobr&quot; will
+            not be held responsible for any consequences. 5. Content Guidelines
+            a. Inappropriate Content: Users are prohibited from sharing
+            explicit, offensive, or illegal content. b. Copyright Compliance:
+            Respect copyrights and permissions when sharing content. 6. Privacy
+            and Data Usage a. Privacy Commitment: &quot;nobr&quot; values user
             privacy. More details can be found in our Privacy Policy. 7.
-            Platform Modifications "nobr" reserves the right to make changes or
-            updates to the platform as necessary. 8. Account Termination Users
-            found wanting breaching the terms of service will most likely be
-            terminated suspended or even banned regarding to the offense
-            committed. 9. Disclaimers a. "nobr" is provided on an "as-is" basis.
-            We do not guarantee the accuracy, availability, or security of the
-            platform. b. Users are solely responsible for their actions and
-            interactions on "nobr." 11. Limitation of Liability "nobr" will not
-            be held liable for any damages, scams, or adverse events that may
-            occur during the use of our platform. 12. Legal Framework These
-            terms are governed by the Nigerian laws and regulations 13. Contact
-            Us If you have any questions or concerns, please do not hesitate to
-            contact us on
+            Platform Modifications &quot;nobr&quot; reserves the right to make
+            changes or updates to the platform as necessary. 8. Account
+            Termination Users found wanting breaching the terms of service will
+            most likely be terminated suspended or even banned regarding to the
+            offense committed. 9. Disclaimers a. &quot;nobr&quot; is provided on
+            an &quot;as-is&quot; basis. We do not guarantee the accuracy,
+            availability, or security of the platform. b. Users are solely
+            responsible for their actions and interactions on &quot;nobr.&quot;
+            11. Limitation of Liability &quot;nobr&quot; will not be held liable
+            for any damages, scams, or adverse events that may occur during the
+            use of our platform. 12. Legal Framework These terms are governed by
+            the Nigerian laws and regulations 13. Contact Us If you have any
+            questions or concerns, please do not hesitate to contact us on
           </p>
         </section>
         <div id="privacy" className="h-20 w-full"></div>
         <section>
           <h1 className="text-2xl font-medium">Privacy</h1>
           <p>
-            Privacy Policy 1. Introduction "nobr" is committed to protecting the
-            privacy of its users. This Privacy Policy outlines our practices
-            regarding the collection, use, and disclosure of personal
-            information on our web-based platform. 2. Anonymous Communication
-            "nobr" provides a platform for anonymous text and video chatting. We
-            do not require users to provide personal information, and we do not
-            actively collect any identifiable data. 3. User Consent By using
-            "nobr," users implicitly consent to the anonymous nature of the
-            platform and acknowledge that the choice to disclose any personal
-            information is at their own risk. 4. Data Collection and Usage As an
-            anonymous platform, "nobr" does not collect or store personal
-            information. Conversations within the platform are not monitored or
-            stored by us. 5. User Conduct and Anonymity Users are encouraged to
-            abide by the community guidelines and terms of service, respecting
-            the anonymity of others and refraining from attempting to uncover or
-            expose the identities of fellow users. 6. Third-Party Services
-            "nobr" does not engage in the sharing of user data with third-party
-            services. We prioritize the confidentiality and privacy of our
-            users. 7. Security Measures While "nobr" takes reasonable measures
-            to ensure the security of the platform, users should be aware that
-            the anonymous nature of the platform inherently carries some risk,
-            and they are advised to exercise caution. 8. Changes to Privacy
-            Policy "nobr" reserves the right to make changes or updates to this
-            Privacy Policy. Users are encouraged to review this policy
+            Privacy Policy 1. Introduction &quot;nobr&quot; is committed to
+            protecting the privacy of its users. This Privacy Policy outlines
+            our practices regarding the collection, use, and disclosure of
+            personal information on our web-based platform. 2. Anonymous
+            Communication &quot;nobr&quot; provides a platform for anonymous
+            text and video chatting. We do not require users to provide personal
+            information, and we do not actively collect any identifiable data.
+            3. User Consent By using &quot;nobr,&quot; users implicitly consent
+            to the anonymous nature of the platform and acknowledge that the
+            choice to disclose any personal information is at their own risk. 4.
+            Data Collection and Usage As an anonymous platform, &quot;nobr&quot;
+            does not collect or store personal information. Conversations within
+            the platform are not monitored or stored by us. 5. User Conduct and
+            Anonymity Users are encouraged to abide by the community guidelines
+            and terms of service, respecting the anonymity of others and
+            refraining from attempting to uncover or expose the identities of
+            fellow users. 6. Third-Party Services &quot;nobr&quot; does not
+            engage in the sharing of user data with third-party services. We
+            prioritize the confidentiality and privacy of our users. 7. Security
+            Measures While &quot;nobr&quot; takes reasonable measures to ensure
+            the security of the platform, users should be aware that the
+            anonymous nature of the platform inherently carries some risk, and
+            they are advised to exercise caution. 8. Changes to Privacy Policy
+            &quot;nobr&quot; reserves the right to make changes or updates to
+            this Privacy Policy. Users are encouraged to review this policy
             periodically for any modifications. 9. Contact Information For
             questions or concerns regarding this Privacy Policy or the
-            platform's practices, users can contact us on gmail at ………. And also
-            on twitter @ nobr_hq By using "nobr," users agree to the terms
-            outlined in this Privacy Policy. It is essential for users to
-            familiarize themselves with this policy to ensure their own safety
-            as nobr wont be held liable for any ill happening
+            platform&apos;s practices, users can contact us on gmail at ………. And
+            also on twitter @ nobr_hq By using &quot;nobr,&quot; users agree to
+            the terms outlined in this Privacy Policy. It is essential for users
+            to familiarize themselves with this policy to ensure their own
+            safety as nobr wont be held liable for any ill happening
           </p>
         </section>
         <div id="community" className="h-20 w-full"></div>
         <section>
           <h1 className="text-2xl font-medium">Community Guidelines</h1>
           <p>
-            Community Guidelines for "NOBR" 1.Respect and Kindness - Treat all
-            users with respect and kindness, regardless of their age, race,
-            gender, or ethnicity. - Avoid engaging in any form of discriminatory
-            or offensive behavior 2.Anonymity Etiquette - Embrace the anonymity
-            feature responsibly and respect the choice of others to remain
-            anonymous. - Do not attempt to uncover or expose the identities of
-            other users against their wishes. 3.Responsible Sharing - Share
-            thoughts, ideas, and content responsibly, keeping in mind the
-            potential impact on others. - Refrain from spreading false
+            Community Guidelines for &quot;NOBR&quot; 1.Respect and Kindness -
+            Treat all users with respect and kindness, regardless of their age,
+            race, gender, or ethnicity. - Avoid engaging in any form of
+            discriminatory or offensive behavior 2.Anonymity Etiquette - Embrace
+            the anonymity feature responsibly and respect the choice of others
+            to remain anonymous. - Do not attempt to uncover or expose the
+            identities of other users against their wishes. 3.Responsible
+            Sharing - Share thoughts, ideas, and content responsibly, keeping in
+            mind the potential impact on others. - Refrain from spreading false
             information or engaging in harmful rumors. 4.Zero Tolerance for
             Harassment - Harassment, cyberbullying, or any form of malicious
             intent towards others is strictly prohibited and would lead to been
@@ -105,16 +107,17 @@ const page = () => {
             or harmful behavior, report it - Provide support to fellow users and
             help maintain a positive community atmosphere. 8.Compliance with
             Terms of Service - Familiarize yourself with and adhere to the
-            "nobr" Terms of Service at all times. 9.Legal Consequences -
-            Understand that illegal activities are not tolerated on the platform
-            and may result in legal actions. - Any criminal activities will be
-            reported to the relevant authorities. 10.Participation is Voluntary
-            - Users have the freedom to participate in conversations and
-            activities on "nobr." - If a conversation makes you uncomfortable,
-            you can disengage or report it. These community guidelines are meant
-            to create a respectful, safe, and inclusive environment for all
-            users of "nobr." Users are expected to abide by these guidelines and
-            report any violations to ensure the well-being of the community.
+            &quot;nobr&quot; Terms of Service at all times. 9.Legal Consequences
+            - Understand that illegal activities are not tolerated on the
+            platform and may result in legal actions. - Any criminal activities
+            will be reported to the relevant authorities. 10.Participation is
+            Voluntary - Users have the freedom to participate in conversations
+            and activities on &quot;nobr.&quot; - If a conversation makes you
+            uncomfortable, you can disengage or report it. These community
+            guidelines are meant to create a respectful, safe, and inclusive
+            environment for all users of &quot;nobr.&quot; Users are expected to
+            abide by these guidelines and report any violations to ensure the
+            well-being of the community.
           </p>
         </section>
       </main>
