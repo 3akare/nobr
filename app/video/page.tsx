b6c6cabc6a2cb20fbb46d484070fc16522b9598a
David Bakare <bakaredavid007@gmail.com>
2023-11-20 01:00:42 +0100

    User Feedback components

---
+++ app/video/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="h-[calc(100vh-228px)] flex items-center justify-center">
+      <p className="text-2xl">Coming Soon...</p>
+    </main>
+  );
+};
+
+export default page;
