664c89f50decb0c8bf26eb14087b9ade63e8be5f
David Bakare <bakaredavid007@gmail.com>
2023-12-16 08:07:17 +0100

    Refactor chat page component to use dynamic messages array
    This commit refactors the chat page component to use a dynamic array of messages instead of a static array. It also adds a handleSubmit function to handle message submission.

--- app/(chat)/chat/[id]/page.tsx
+++ app/(chat)/chat/[id]/page.tsx
@@ -1,20 +1,23 @@
+"use client";
 // Import necessary components
 import { ChatBody } from "../../components";
 
 // Define a constant array for messages
-const messages = ["This", "Space", "Isn't", "ready", "yet"];
+const messages = [
+  { messages: "hello", timestamp: Date.now() },
+  { messages: "Get the ", timestamp: Date.now() },
+  { messages: "Fuck", timestamp: Date.now() },
+  { messages: "Out of", timestamp: Date.now() },
+  { messages: "Here", timestamp: Date.now() },
+];
+const handleSubmit = (value: string) => {
+  console.log(`${value}`);
+};
 
 // Define the Page component
 // It renders a ChatBody component with a messages prop
 const Page = () => {
-  return (
-    <ChatBody
-      messages={messages}
-      onMessageSubmit={() => {
-        console.log("Hello world");
-      }}
-    />
-  );
+  return <ChatBody messages={messages} onMessageSubmit={handleSubmit} />;
 };
 
 // Export the Page component as default
